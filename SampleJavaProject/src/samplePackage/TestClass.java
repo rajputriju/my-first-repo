e7921e1b78b5091faa121ae64e63654d18bbb6ca
rajpu <rajpu@DESKTOP-ST59AK5>
2022-08-20 23:42:44 +0530

    New change

--- SampleJavaProject/src/samplePackage/TestClass.java
+++ SampleJavaProject/src/samplePackage/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String args[]) {
 		System.out.println("Hello There");
 		System.out.println("Hello This line is written in Github");
+		System.out.println("Hello This line is written in eclipse");
 	}
 
 }
